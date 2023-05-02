0d5bedb59bc6be78fd85a87496fcda96b36feba0
Alexis Amadei <alexisamadei@gmail.com>
2023-05-02 22:15:09 +0200

    update

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import "../css/AppBar.css";
+
+import ACCOUNT from "../assets/account.svg";
+import MAP from "../assets/map.svg";
+import PIN from "../assets/pin.svg";
+import BOLT from "../assets/bolt.svg";
+import CREDIT_CARD from "../assets/creditCard.svg";
+
 export default function AppBar() {
-    return (
-        <div>
-            <h1>AppBar</h1>
-        </div>
-    )
+  return (
+    <div className="navbar-container">
+      <ol className="menu-list">
+        <li className="menu-item"><img src={PIN} alt="Pin" />Stations</li>
+        <li className="menu-item"><img src={MAP} alt="Map" />Itinéraires</li>
+        <li className="menu-item"><img src={BOLT} alt="Charge" />Charge</li>
+        <li className="menu-item"><img src={CREDIT_CARD} alt="Credit Card" />Ma Carte</li>
+        <li className="menu-item"><img src={ACCOUNT} alt="Account" />Compte</li>
+      </ol>
+    </div>
+  )
 }
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import AppBar from "../components/AppBar";
+
 export default function Landing() {
     return (
         <div className="view-wrapper">
+            <AppBar />
             <h1>Landing</h1>
         </div>
     );
