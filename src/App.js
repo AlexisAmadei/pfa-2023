23b976ffac668dd42041d0b06d16129f7393ef59
Alexis Amadei <alexisamadei@gmail.com>
2023-05-02 19:12:24 +0200

    start app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css';
 import Landing from './views/Landing';
 import Itinerary from './views/Itinerary';
 import Charge from './views/Charge';
 import Card from './views/Card';
 import Account from './views/Account';
 
+import './App.css';
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route to />
-        <Route path='/' element={ <Landing />} />
-        <Route path='/itinerary' element={ <Itinerary /> } />
-        <Route path='/charge' element={ <Charge /> } />
-        <Route path='/card' element={ <Card /> } />
-        <Route path='/account' element={ <Account /> } />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/itinerary' element={<Itinerary />} />
+          <Route path='/charge' element={<Charge />} />
+          <Route path='/card' element={<Card />} />
+          <Route path='/account' element={<Account />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
