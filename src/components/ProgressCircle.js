2fe5dbbe50a76ec6717301180598e45afa314355
Alexis Amadei <alexisamadei@gmail.com>
2023-05-12 20:53:16 +0200

    edit progress circle component

--- src/components/ProgressCircle.js
+++ src/components/ProgressCircle.js
@@ -3,11 +3,10 @@ import React from 'react';
 import './css/ProgressCircle.css';
 
 const ProgressCircle = (props) => {
-  const { percentage, circleWidth, max } = props;
+  const { percentage, circleWidth, current } = props;
   const radius = 85;
   const dashArray = radius * Math.PI * 2;
   const dashOffset = dashArray - (dashArray * percentage) / 100;
-  const backDashOffset = dashArray - (dashArray * max) / 100;
 
   return (
     <div>
@@ -16,18 +15,13 @@ const ProgressCircle = (props) => {
         height={circleWidth}
         viewBox={`0 0 ${circleWidth} ${circleWidth}`}
       >
-        {/* <circle
+        <circle
           className='circle-background'
           cx={circleWidth / 2}
           cy={circleWidth / 2}
           strokeWidth={"15px"}
           r={radius}
-          style={{
-            strokeDasharray: dashArray,
-            strokeDashoffset: backDashOffset,
-          }}
-        /> */}
-
+        />
         <circle
           className='circle-progress'
           cx={circleWidth / 2}
@@ -40,13 +34,7 @@ const ProgressCircle = (props) => {
           }}
           transform={`rotate(-90 ${circleWidth / 2} ${circleWidth / 2})`}
         />
-        <text
-          className='circle-text'
-          x='50%'
-          y='50%'
-          dy='.3em'
-          textAnchor='middle'
-        >
+        <text className='circle-text' x='50%' y='50%' dy='.3em' textAnchor='middle'>
           {`${percentage}%`}
         </text>
       </svg>
