c88bd6f4ec497ae8b15d0001966ed79f594e16b3
Alexis Amadei <alexisamadei@gmail.com>
2023-05-17 10:33:52 +0200

    update landing page with text field

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -3,16 +3,27 @@ import React from "react";
 import OSP from "../components/OSMapIntegration";
 import EngieAppBar from "../components/EngieAppBar";
 
+import TextField from '@mui/material/TextField';
+
 import "../css/Landing.css"
 
 export default function Landing() {
   return (
     <div className="landing-container">
-      <div className="landing-footer">
-        <EngieAppBar active="stations" />
-      </div>
       <div className="map-container">
         <OSP />
+      </div>
+      <div className="search-box">
+        <TextField className="address-searchBar" id="outlined-basic" label="Chercher une adresse" variant="outlined"
+          fullWidth={true}
+          sx={{
+            backgroundColor: 'white',
+            border: 'none',
+          }}
+        />
+      </div>
+      <div className="landing-footer">
+        <EngieAppBar active="stations" />
       </div>
     </div>
   );
